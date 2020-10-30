829915aa3f596cddfb37f3a0afef1b25f64bfeb3
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-30 15:21:11 +0530

    Add input box for posts on compose route

--- app.js
+++ app.js
@@ -35,6 +35,11 @@ app.get("/compose", (req, res) =>
 	res.render("compose");
 })
 
+app.post("/compose", (req, res) =>
+{
+	console.log(req.body.content);
+})
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
