e4b43aed79c2d873a5db2cafc308dc718512a2ed
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-30 13:56:24 +0530

    Add home content on root route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+const homeContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,7 +15,7 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) =>
 {
-	res.render("home");
+	res.render("home", {homeContent: homeContent});
 })
 
 app.listen(3000, function() {
